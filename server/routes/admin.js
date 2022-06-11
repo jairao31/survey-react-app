b552647627ef4e110b76e805590aff326f932a4c
Jaiganesh Rao <jai.31rao@gmail.com>
2022-06-10 21:30:14 -0400

    backend in progress-1

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const adminData = data.admin;
+const { v4 } = require("uuid");
 
 router.post("/createSurvey", async (req, res) => {
   const reqData = req.body;
@@ -12,7 +13,8 @@ router.post("/createSurvey", async (req, res) => {
 
 router.post("/createQuestion", async (req, res) => {
   const reqData = req.body;
-  const { qId, question, type, surveyId } = reqData;
+  const { question, type, surveyId } = reqData;
+  qId = v4();
   const output = await adminData.createQuestion(qId, question, type, surveyId);
   res.json(output);
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,11 +2,18 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const adminData = data.user;
+const { v4 } = require("uuid");
 
 router.post("/createAnswer", async (req, res) => {
   const reqData = req.body;
-  const { aId, answer, surveyId, questionId } = reqData;
-  const output = await data.createAnswer(aId, answer, surveyId, questionId);
+  const { answer, surveyId, questionId } = reqData;
+  aId = v4();
+  const output = await adminData.createAnswer(
+    aId,
+    answer,
+    surveyId,
+    questionId
+  );
   res.json(output);
 });
 
