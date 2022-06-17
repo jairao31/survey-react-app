cfbf2a623355500b324556edacf8022ad1adc4c7
Jaiganesh Rao <jai.31rao@gmail.com>
2022-06-17 06:52:31 -0400

    frontend in progress-19

--- client/src/components/User/UserSurveySubmission.js
+++ client/src/components/User/UserSurveySubmission.js
@@ -1,25 +1,111 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Button, Container, ListGroup, Stack } from "react-bootstrap";
+import { Button, Container, ListGroup, Stack, Card } from "react-bootstrap";
 import Navigate from "../Navigate";
 
-import Axios from "axios";
+import axios from "axios";
 
 const UserSurveySubmission = () => {
   const data = useParams();
   const sID = data.sId;
   const uID = data.uId;
 
+  const [userName, setUserName] = useState("");
+
+  const [surveyName, setSurveyName] = useState("");
+  const [surveyDesc, setSurveyDesc] = useState("");
+  const [allQ, setAllQ] = useState([]);
+  const [allQC, setAllQC] = useState({});
+
   useEffect(() => {
-    // console.log(data);
+    if (!uID) return;
+    const getUserName = async (uid) => {
+      const userN = await axios.get(
+        `http://localhost:3001/admin/getUsername/${uid}`
+      );
+      setUserName(userN.data[0].username);
+      //   console.log(userN.data[0].username);
+    };
+    getUserName(uID);
+  }, [sID]);
+
+  useEffect(() => {
+    if (!sID) return;
+    const getSurvey = async (id) => {
+      const { data } = await axios.get(
+        `http://localhost:3001/admin/getAllQuestions/${id}`
+      );
+      setAllQ(data);
+    };
+    getSurvey(sID);
+
+    const getSurveyDetails = async (id) => {
+      const sData = await axios.get(
+        `http://localhost:3001/admin/getSurveyDetailsByID/${id}`
+      );
+      // setSurveyDetails(sData.data);
+      setSurveyName(sData.data[0].name);
+      setSurveyDesc(sData.data[0].description);
+    };
+    getSurveyDetails(sID);
+  }, [sID]);
+
+  useEffect(() => {
+    if (allQ.length === 0) return;
+
+    const getAllQCs = async (sid, qid) => {
+      const { data } = await axios.get(
+        `http://localhost:3001/admin/getAllQchoices/${sid}/${qid}`
+      );
+      return data;
+    };
+
+    allQ.forEach(async (el) => {
+      let data = await getAllQCs(sID, el.qId);
+      setAllQC((prev) => {
+        return {
+          ...prev,
+          [el.qId]: data,
+        };
+      });
+    });
+  }, [allQ]);
+
+  useEffect(() => {
+    // console.log(surveyName);
+    // console.log(surveyDesc);
+    // console.log(allQ);
+    // console.log(allQC);
   });
+
+  //   handleSurveyQ = async (sID) => {};
 
   return (
     <Container>
       <Navigate />
-      {/* <h1>{sID}</h1>
 
-      <h1>{uID}</h1> */}
+      <div className="w-25 fixed">
+        {surveyName && surveyDesc ? (
+          <Stack>
+            <br />
+            <h6>
+              <b>{userName}'s</b> response for:
+            </h6>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>{surveyName}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  ID: "{sID}"
+                </Card.Subtitle>
+                <br />
+                <Card.Text>{surveyDesc}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Stack>
+        ) : (
+          <></>
+        )}
+      </div>
     </Container>
   );
 };
